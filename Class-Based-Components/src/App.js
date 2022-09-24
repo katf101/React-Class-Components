59ce80551d9723d1d99bf5d982bbbbf25e1a9419
Hansol <102638663+katf101@users.noreply.github.com>
2022-09-24 22:44:04 +0900

    context Provider

--- Class-Based-Components/src/App.js
+++ Class-Based-Components/src/App.js
@@ -1,11 +1,22 @@
 import UserFinder from "./components/UserFinder";
+import UsersContext from "./store/users-context";
 
-function App() {
+const DUMMY_USERS = [
+  { id: "u1", name: "한솔" },
+  { id: "u2", name: "정욱" },
+  { id: "u3", name: "장현" },
+];
+
+const App = () => {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
-}
+};
 
 export default App;
